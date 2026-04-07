c507473b0621eb6ec105331d97e0e4fe77cb7507
Linh Hoang <linhoang.ce@gmail.com>
2026-04-06 22:38:18 -0700

    add admin management dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-cente font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
-        <div>Comp4537 - Term Project</div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex flex-col flex-1 items-center justify-cente font-sans dark:bg-black">
+			<main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start">
+				<div>
+					<h1>Comp4537 - Term Project</h1>
+					<h3>AI Phone Agent Demo</h3>
+				</div>
+			</main>
+		</div>
+	);
 }
--- app/user/page.tsx
+++ app/user/page.tsx
@@ -5,34 +5,34 @@ import { useRouter } from "next/navigation";
 import PhoneAgent from "@/components/PhoneAgent";
 
 export default function UserPage() {
-  const [data, setData] = useState(null);
-  const router = useRouter();
+	const [data, setData] = useState(null);
+	const router = useRouter();
 
-  useEffect(() => {
-    fetch("https://comp4537-project-iot-ai-backend.onrender.com/api/user", {
-      credentials: "include", // Ensure cookies are sent with the request
-    })
-      .then((res) => {
-        if (res.status === 401) {
-          // If the session isn't valid, send them back to login
-          window.location.href = "/login";
-        }
-        return res.json();
-      })
-      .then((json) => {
-        if (json && json.user) {
-          setData(json.user);
-        }
-      })
-      .catch((err) => console.error("Fetch error:", err));
-  }, []);
+	useEffect(() => {
+		fetch("https://comp4537-project-iot-ai-backend.onrender.com/api/user", {
+			credentials: "include", // Ensure cookies are sent with the request
+		})
+			.then((res) => {
+				if (res.status === 401) {
+					// If the session isn't valid, send them back to login
+					window.location.href = "/login";
+				}
+				return res.json();
+			})
+			.then((json) => {
+				if (json && json.user) {
+					setData(json.user);
+				}
+			})
+			.catch((err) => console.error("Fetch error:", err));
+	}, []);
 
-  return (
-	<div>
-		<div className="text-2xl font-bold mb-4">
-			 {data ? `Welcome to the ${data}` : "Loading..."}
+	return (
+		<div>
+			<div className="text-2xl font-bold mb-4">
+				{data ? `Welcome to the ${data}` : "Loading..."}
 			</div>
 			<PhoneAgent />
-		
-		</div>);
+		</div>
+	);
 }
